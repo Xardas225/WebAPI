0fe485360f090c4d1c0db32d066dff3d266fd6e6
Alexander Derevkov <alexanderderevkov@yandex.ru>
2026-02-19 18:55:57 +0300

    add redis to ingredients

--- Services/IngredientsService.cs
+++ Services/IngredientsService.cs
@@ -1,21 +1,45 @@
-﻿using WebAPI.Models.Dish.Ingredient;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using WebAPI.Models.Dish.Ingredient;
+using WebAPI.Models.Dish.Kitchens;
 using WebAPI.Repositories.Interfaces;
 using WebAPI.Services.Interfaces;
+using System.Text.Json;
 
 namespace WebAPI.Services;
 
 public class IngredientsService : IIngredientsService
 {
-    private readonly IIngredientsRepository _ingredientsRepository;
+    private const string ingredientsCacheKey = "ingredientsKey";
 
-    public IngredientsService(IIngredientsRepository ingredientsRepository)
+    private readonly IIngredientsRepository _ingredientsRepository;
+    private readonly IDistributedCache _cache;
+
+    public IngredientsService(IIngredientsRepository ingredientsRepository, IDistributedCache cache)
     {
         _ingredientsRepository = ingredientsRepository;
+        _cache = cache;
     }
 
     public async Task<List<IngredientEntity>> GetAllIngredientsAsync()
     {
-        return await _ingredientsRepository.GetAllIngredients();
+        var cachedData = await _cache.GetStringAsync(ingredientsCacheKey);
+
+
+        if (!string.IsNullOrEmpty(cachedData))
+        {
+            return JsonSerializer.Deserialize<List<IngredientEntity>>(cachedData);
+        }
+
+        var response = await _ingredientsRepository.GetAllIngredients();
+
+        var options = new DistributedCacheEntryOptions
+        {
+            SlidingExpiration = TimeSpan.FromHours(1)
+        };
+
+        await _cache.SetStringAsync(ingredientsCacheKey, JsonSerializer.Serialize(response), options);
+
+        return response;
     }
 
     public async Task<IngredientEntity> GetIngredientByIdAsync(int id)
